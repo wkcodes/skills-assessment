00c4c92e426f4cc1921f3a0172ddb5422207638b
wkcodes <wkcodes2020@gmail.com>
2022-02-15 21:52:47 -0800

    added the general layout

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log('test');
+const contacts = [
+  {
+    name: 'Christian',
+    email: 'christain@yahoo.com',
+    phone: '323-555-1234',
+    address: '563 Waterfront Place Oakland CA 94653',
+  },
+];
